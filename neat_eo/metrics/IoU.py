7cad48d080a3dd15663752d1f3392940725c919f
Wessel93 <wes_dejong9@hotmail.com>
2020-08-21 14:02:11 +0200

    Return 1 instead of NAN with empty image

--- neat_eo/metrics/IoU.py
+++ neat_eo/metrics/IoU.py
@@ -5,12 +5,14 @@ def get(label, predicted, config=None):
 
     tn, fn, fp, tp = confusion(label, predicted)
     if tp == 0 and fp == 0 and fn == 0:
-        return float("NaN")
+        return 1
+        #return float("NaN")
 
     try:
         assert tp or fp or fn
         iou = float(tp / (fp + fn + tp))
     except ZeroDivisionError:
-        iou = float("NaN")
+        iou = 1
+        #iou = float("NaN")
 
     return iou
