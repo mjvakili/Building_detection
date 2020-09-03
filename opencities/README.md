5cfd9ca5791664c06543f0b017711aa02cf20a02
mjvakili <mjvakili@nyu.edu>
2020-09-03 16:19:26 +0200

    download and preprocess opencities-AI

---
+++ opencities/README.md
@@ -0,0 +1,9 @@
+# Stand-alone script for downloading and preprocessing the open-cities data
+
+##requirements:
+
+rasterio 
+
+pandas
+
+
---
+++ opencities/preprocess_test.sh
@@ -0,0 +1,29 @@
+echo "creating place-holders for the raw and processed files"
+RAW_DATA_DIR=DATA/raw
+INTER_DATA_DIR=DATA/interim
+PROCESSED_DATA_DIR=DATA/processed
+
+mkdir -p $RAW_DATA_DIR
+mkdir -p $INTER_DATA_DIR
+mkdir -p $PROCESSED_DATA_DIR
+
+echo "Downloading the test mosaic file"
+wget https://raw.githubusercontent.com/qubvel/open-cities-challenge/master/data/processed/test_mosaic.csv
+
+echo "Downloading the test data"
+wget https://drivendata-public-assets.s3.amazonaws.com/test.tgz
+
+
+mv test.tgz $RAW_DATA_DIR
+mv test_mosaic.csv $PROCESSED_DATA_DIR
+
+
+echo "Extracting test files..."
+tar -xvzf $RAW_DATA_DIR/test.tgz -C $INTER_DATA_DIR
+
+
+echo "Stitching test files..."
+python -m processor.stitch_test \
+    --df_path=$PROCESSED_DATA_DIR/test_mosaic.csv \
+    --path_pattern=$INTER_DATA_DIR/test/*/*.tif \
+    --dst_dir=$PROCESSED_DATA_DIR/test
