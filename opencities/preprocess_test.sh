89f2d51d1d62ac8d6448f44daa32c6686c2b5895
Mohammadjavad Vakili <mv1003@nyu.edu>
2020-10-22 20:50:03 +0200

    typo

--- opencities/preprocess_test.sh
+++ opencities/preprocess_test.sh
@@ -1,3 +1,6 @@
+echo "installing rasterio"
+pip3 install --user rasterio
+
 echo "creating place-holders for the raw and processed files"
 RAW_DATA_DIR=DATA/raw
 INTER_DATA_DIR=DATA/interim
@@ -8,22 +11,21 @@ mkdir -p $INTER_DATA_DIR
 mkdir -p $PROCESSED_DATA_DIR
 
 echo "Downloading the test mosaic file"
-wget https://raw.githubusercontent.com/qubvel/open-cities-challenge/master/data/processed/test_mosaic.csv
+wget https://raw.githubusercontent.com/drivendataorg/open-cities-ai-challenge/master/1st%20Place/data/processed/test_mosaic.csv 
 
 echo "Downloading the test data"
 wget https://drivendata-public-assets.s3.amazonaws.com/test.tgz
 
-
 mv test.tgz $RAW_DATA_DIR
-mv test_mosaic.csv $PROCESSED_DATA_DIR
 
+mv test_mosaic.csv $PROCESSED_DATA_DIR
 
 echo "Extracting test files..."
 tar -xvzf $RAW_DATA_DIR/test.tgz -C $INTER_DATA_DIR
 
 
 echo "Stitching test files..."
-python -m processor.stitch_test \
+python3 -m preprocessor.stitch_test \
     --df_path=$PROCESSED_DATA_DIR/test_mosaic.csv \
     --path_pattern=$INTER_DATA_DIR/test/*/*.tif \
     --dst_dir=$PROCESSED_DATA_DIR/test
