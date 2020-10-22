fdb922fd5b83d038fe10fcce731bbac270ce01e1
Mohammadjavad Vakili <mv1003@nyu.edu>
2020-10-22 20:51:49 +0200

    install dependencies

--- opencities/preprocess_train.sh
+++ opencities/preprocess_train.sh
@@ -1,3 +1,7 @@
+echo "Installing the necessary libraries"
+conda install -c conda-forge gdal
+pip install aeronet --user
+
 echo "creating place-holders for the raw and processed files"
 
 RAW_DATA_DIR=DATA/raw
@@ -7,21 +11,23 @@ PROCESSED_DATA_DIR=DATA/processed
 echo "Downloading the train tier 1 data"
 wget https://drivendata-public-assets-eu.s3.eu-central-1.amazonaws.com/train_tier_1.tgz
 
-# unpack data archives
+mv train_tier_1.tgz $RAW_DATA_DIR
+
 echo "Extracting train tier 1 files..."
 tar -xvzf $RAW_DATA_DIR/train_tier_1.tgz -C $INTER_DATA_DIR
 
+
 # resample train data to 0.1 meters per pixel
 echo "Resamping train data to 0.1 m/pixel"
-python -m preprocessor.resample --path_pattern=$INTER_DATA_DIR/train_tier_1/*/*/*.tif --dst_res=0.1
+python3 -m preprocessor.resample --path_pattern=$INTER_DATA_DIR/train_tier_1/*/*/*.tif --dst_res=0.1
 
 # for each resampled tif file create raster mask from provided geometries in .geojson file
 echo "Creating raster masks"
-python -m preprocessor.generate_masks --path_pattern=$INTER_DATA_DIR/train_tier_1/*/*/res-*.tif
+python3 -m preprocessor.generate_masks --path_pattern=$INTER_DATA_DIR/train_tier_1/*/*/res-*.tif
 
 # slicing big tif files to small ones with size 1024x1024
 echo "Slicing tif files..."
-python -m preprocessor.cut_train \
+python3 -m preprocessor.cut_train \
     --path_pattern=$INTER_DATA_DIR/train_tier_1/*/*/res-*.tif \
     --dst_dir=$PROCESSED_DATA_DIR/train \
     --sample_size=1024
